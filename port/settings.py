eb39be21ef26e1c7d2e5b063cd00b42f170b8164
Vandann-1 <vandanparmar6041@gmail.com>
2025-04-13 12:39:47 +0530

    Jkk

--- port/settings.py
+++ port/settings.py
@@ -42,8 +42,9 @@ INSTALLED_APPS = [
 
 import os
 
-MEDIA_URL = '/media/'  
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')  
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
--- port/urls.py
+++ port/urls.py
@@ -25,6 +25,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',home ,name='home'),
     
-]
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
